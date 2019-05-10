e2d80ffce2d116b0cdb439c8fff2cf6435bb2c19
Alec Barton <abartondeveloper@gmail.com>
2019-05-10 10:33:10 -0400

    Initial Commit

---
+++ TT2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TT2
+//
+//  Created by Alec Barton on 5/10/19.
+//  Copyright © 2019 Alec Barton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TT2Tests/TT2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  TT2Tests.swift
+//  TT2Tests
+//
+//  Created by Alec Barton on 5/10/19.
+//  Copyright © 2019 Alec Barton. All rights reserved.
+//
+
+import XCTest
+@testable import TT2
+
+class TT2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
